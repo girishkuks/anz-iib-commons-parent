146e78b998af2d6bfc61f582b8e3b084939302a2
root <root@localhost>
2016-05-16 10:00:20 +1000

    Handle null transformer

--- anz-iib-commons/src/main/java/com/anz/common/compute/impl/CommonBlobTransformCompute.java
+++ anz-iib-commons/src/main/java/com/anz/common/compute/impl/CommonBlobTransformCompute.java
@@ -10,66 +10,54 @@ import com.ibm.broker.plugin.MbMessageAssembly;
 
 /**
  * @author sanketsw
- *
+ * 
  */
 public abstract class CommonBlobTransformCompute extends CommonJavaCompute {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.anz.common.compute.ICommonComputeNode#getTransformationType()
 	 */
 	public TransformType getTransformationType() {
 		return TransformType.HTTP_HHTP;
 	}
-	
-	/* (non-Javadoc)
-	 * @see com.anz.common.compute.impl.CommonJavaCompute#execute(com.ibm.broker.plugin.MbMessageAssembly, com.ibm.broker.plugin.MbMessageAssembly)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.anz.common.compute.impl.CommonJavaCompute#execute(com.ibm.broker.
+	 * plugin.MbMessageAssembly, com.ibm.broker.plugin.MbMessageAssembly)
 	 */
 	@Override
 	public void execute(MbMessageAssembly inAssembly,
 			MbMessageAssembly outAssembly) throws Exception {
 		
-		MbMessage inMessage = inAssembly.getMessage();
-		MbMessage outMessage = outAssembly.getMessage();
-		
-		String inputString = ComputeUtils.getStringFromBlob(inMessage);
-		String outputString = executeBlobTranform(inputString);
-		if (outputString != null) {
-			// Write this outputJson to outMessage
-			ComputeUtils.replaceStringAsBlob(outMessage, outputString);
+		ITransformer<String, String> stringTransformer = getTransformer();
+		if (stringTransformer != null) {
+
+			MbMessage inMessage = inAssembly.getMessage();
+			MbMessage outMessage = outAssembly.getMessage();
+
+			String inputString = ComputeUtils.getStringFromBlob(inMessage);
+
+			String outputString = stringTransformer.execute(inputString,
+					appLogger, metaData);
+			
+			if (outputString != null) {
+				// Write this outputJson to outMessage
+				ComputeUtils.replaceStringAsBlob(outMessage, outputString);
+			}
 		}
-		
+
 	}
-	
-	/**
-	 * Write business logic here if you merely need to transform the message
-	 * from JSON to JSON
-	 * 
-	 * @param inputJson
-	 *            input JSON Data
-	 * @return output JSON Data to be placed in the message
-	 */
-	public String executeBlobTranform(String inputJson) throws Exception {
-		String out = null;
-		//String transformerClassName = getName().substring(getName().indexOf("com"));
-		//logger.info("Creating instance of {}", transformerClassName);
-		try {
-			//ITransformer<String, String> jsonTransformer = (ITransformer<String, String>)Class.forName(transformerClassName).newInstance();
-			ITransformer<String, String> stringTransformer = getTransformer();
-			out = stringTransformer.execute(inputJson, appLogger, metaData);
-		} catch(Exception e) {
-			logger.throwing(e);
-			throw e;
-		}
-		return out;
-	}
-	
+
 	/**
 	 * Get the external transformer class instance
+	 * 
 	 * @return
 	 */
 	public abstract ITransformer<String, String> getTransformer();
-
-	
-	
 
 }
